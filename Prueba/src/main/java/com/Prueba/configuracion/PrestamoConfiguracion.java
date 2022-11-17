a5240f6edc7855ea5dcf2f067a897b2d14c13445
mromerop92 <antonioromeropereira@yahoo.es>
2022-11-16 18:01:16 -0600

    prueba numero dos 16112022

--- Prueba/src/main/java/com/Prueba/configuracion/PrestamoConfiguracion.java
+++ Prueba/src/main/java/com/Prueba/configuracion/PrestamoConfiguracion.java
@@ -1,5 +1,6 @@
-package com.Prueba;
+package com.Prueba.configuracion;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -9,13 +10,19 @@ import javax.sql.DataSource;
 @Configuration
 public class PrestamoConfiguracion {
 
+    @Value("{usr}")
+    private String usr;
+
+    @Value("{cls}")
+    private String clss;
+
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setUrl("jdbc:sqlserver://192.168.10.136;database=master;trustServerCertificate=true");
-        dataSource.setUsername("UCEM_IRENE");
-        dataSource.setPassword("1234");
+        dataSource.setUsername(usr);
+        dataSource.setPassword(clss);
 
         return dataSource;
     }
